a989b49bda7da8ad9d6100fe5e1d77f4a15c94b8
bharath <bharathcg7@gmail.com>
2024-10-24 12:53:35 -0700

    first commit

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "76589609-f356-45d6-9343-cacd37492e24"
+  subscription_id = "97d62e53-6132-48c5-a9be-83072204c44c"
 }
 
 # Define the resource group
-resource "azurerm_resource_group" "Cloudlearning" {
-  name     = "Cloudlearning"
+resource "azurerm_resource_group" "mrcloud" {
+  name     = "mrcloud"
   location = "UKSouth"
 }
